a15e8d6ace18a30a0f882afecdb60b328a927df2
R3dNailzREH <steelref23@gmail.com>
2020-10-24 22:06:33 -0400

    added support for specifying logfile name on commmand line

--- check_duplicate/check_duplicates.py
+++ check_duplicate/check_duplicates.py
@@ -21,21 +21,40 @@ Maintains a list of files that have already been downloaded in a file
 called history.log.
 
 LIMITATIONS:
-* It must exist the folder where the script is run
+* Script must exist the folder where the script is run
+* Only checks filename for dup, doesn't compare file size
+* If the file is already in /duplicates the script will fail if it tries to
+  to move another file with the same name into the /duplicates folder
 
 FUTURE:
-*  Specify the log file
+* Add support for file size
+* Add support for duplication in the /duplicates folder
 """
 
 # The log file contains all the files downloaded from a location
 # format:
-import csv
 import os
 import shutil
 import sys
 
-# TODO: Make this a command line parameter
-lsLogFileName = "youtube.log"
+# If they don't give a logfile name, tell them it's required and exit
+if len(sys.argv) < 2:
+    print("Usage:\n  python check_duplicates LOGFILE_NAME")
+    exit(1)
+
+# If they give a name that doesn't exist, ask if they want to create it
+# If they do, create an empty file with that name so we can let the open
+# routine run and read no keys.
+lsLogFileName = sys.argv[1]
+if not os.path.isfile(lsLogFileName):
+    print("File not found: ", lsLogFileName)
+    lsKeypress = input("To create a new logfile, press y, or any other character to abort: ")
+    if ("y" == lsKeypress or "Y" == lsKeypress):
+        with open(lsLogFileName, 'w') as fp:
+            pass
+    else:
+        exit(1)
+
 lsBackupFileName = lsLogFileName + '.bak'
 lnNewFiles = 0
 lnDupFiles = 0
@@ -52,12 +71,14 @@ for lsCurrentReadLine in lcLogFileHandle:
         lcAlreadyDownloadedFileDictionary[laCurrentLineSplit[0]] = laCurrentLineSplit[1]
 
 # get a list of files in the current folder
-# TODO: Make this a command line parm
 lsFolderName = "."
 laListOfFiles = os.listdir(lsFolderName)
 
 # If filename is in the dictionary move it to the duplicates folder
 # If it's not in the list, add it to the list
+
+# TODO: It tracks the file size but I'll need a strat for dupkeys.
+#       maybe just filename_size as the key and not use a dict?
 for lsFileEntry in laListOfFiles:
     sys.stdout.write("\nCurrent file: ")
     sys.stdout.write(lsFileEntry)
@@ -66,10 +87,11 @@ for lsFileEntry in laListOfFiles:
             or lsFileEntry == "check_duplicates.py"):
         continue
     if (os.path.isdir(lsFileEntry)):
+        sys.stdout.write(" is Folder")
         continue
     if lsFileEntry in lcAlreadyDownloadedFileDictionary.keys():
         lnDupFiles = lnDupFiles + 1
-        sys.stdout.write(" is Duplicate")
+        sys.stdout.write(" is Duplicate --> Move to /duplicates")
         lsDupFolderName = "duplicates"
         if not os.path.isdir(lsDupFolderName):
             os.mkdir(lsDupFolderName)
@@ -78,8 +100,9 @@ for lsFileEntry in laListOfFiles:
         lnNewFiles = lnNewFiles + 1
         sys.stdout.write(" NEW  ")
         # TODO: Add the file size to the check just in case
-        print("key: ", lsFileEntry, " value: ", 1)
-        lcAlreadyDownloadedFileDictionary[lsFileEntry] = "1"
+        lnFileSize = os.path.getsize(lsFileEntry)
+        print("key: ", lsFileEntry, " value: ", lnFileSize)
+        lcAlreadyDownloadedFileDictionary[lsFileEntry] = lnFileSize
 
 # Save dictionary back out to file
 # Create backup
@@ -92,7 +115,7 @@ shutil.copyfile(lsLogFileName, lsBackupFileName)
 with open(lsLogFileName, 'w') as lcNewLogFileHandle:
     # print(lcAlreadyDownloadedFileDictionary, file=lcNewLogFileHandle)
     for lsKey in lcAlreadyDownloadedFileDictionary.keys():
-        lsTempString = lsKey + ',' + lcAlreadyDownloadedFileDictionary[lsKey]
+        lsTempString = lsKey + ',' + str(lcAlreadyDownloadedFileDictionary[lsKey])
         print(lsTempString, file=lcNewLogFileHandle)
 
 # Print stats
